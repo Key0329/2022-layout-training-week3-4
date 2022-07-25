0c3a7b566b0734578b69267b4464fb83413c81ce
Key <springfield0329@gmail.com>
2022-07-25 12:28:58 +0800

    ham slideToggle

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,11 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$(document).ready(function(){
+    //下拉選單 
+    $(".menu").click(function(event){
+        event.preventDefault();
+        // $(".menu-drop").toggleClass("d-sm-flex");
+        $(".menu-drop").slideToggle(function() {
+            if ($(this).is(":visible"))
+                $(this).css("display","flex");
+        });
+    });
 });
